8f69404f254179809c50e2f4536623f0a872d939
Cydiater <cydiater@gmail.com>
2024-06-06 23:58:42 +0800

    Initial Commit

---
+++ matchedgeometryeffect/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  matchedgeometryeffect
+//
+//  Created by Cydiater on 6/6/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ matchedgeometryeffect/matchedgeometryeffectApp.swift
@@ -0,0 +1,17 @@
+//
+//  matchedgeometryeffectApp.swift
+//  matchedgeometryeffect
+//
+//  Created by Cydiater on 6/6/2024.
+//
+
+import SwiftUI
+
+@main
+struct matchedgeometryeffectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
